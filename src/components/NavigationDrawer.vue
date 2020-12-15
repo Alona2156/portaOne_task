c2075f058ffbe51b9846553828fa3a000716f2f2
Alona <alona2156fr@gmail.com>
2020-12-15 19:17:20 +0200

    refactor: nav drawer with real links

--- src/components/NavigationDrawer.vue
+++ src/components/NavigationDrawer.vue
@@ -1,23 +1,13 @@
 <template>
-  <v-card height="400" width="256" class="mx-auto">
+  <v-card height="400" width="256" class="mx-auto nav-drawer">
     <v-navigation-drawer permanent>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title"> Application </v-list-item-title>
-          <v-list-item-subtitle> subtext </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
       <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.link">
           <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
+            <v-icon class="nav-drawer__icon">{{ item.icon }}</v-icon>
           </v-list-item-icon>
-
           <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item-title class="nav-drawer__title">{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -30,13 +20,11 @@ import Vue from 'vue';
 import {
   VCard,
   VNavigationDrawer,
-  VDivider,
   VList,
   VListItem,
   VListItemIcon,
   VListItemContent,
   VListItemTitle,
-  VListItemSubtitle,
   VIcon,
 } from 'vuetify/lib';
 
@@ -44,21 +32,19 @@ export default Vue.extend({
   components: {
     VCard,
     VNavigationDrawer,
-    VDivider,
     VList,
     VListItem,
     VListItemIcon,
     VListItemContent,
     VListItemTitle,
-    VListItemSubtitle,
     VIcon,
   },
   data() {
     return {
       items: [
-        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-        { title: 'Photos', icon: 'mdi-image' },
-        { title: 'About', icon: 'mdi-help-box' },
+        { title: 'Posts', icon: 'mdi-post', link: '/posts' },
+        { title: 'Photos', icon: 'mdi-image', link: '/photos' },
+        { title: 'Settings', icon: 'mdi-cogs', link: '/settings' },
       ],
       right: null,
     };
@@ -67,4 +53,11 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
+  .nav-drawer__icon.v-icon {
+    margin-right: 32px;
+    color: rgba(0, 0, 0, 0.54);
+  }
+  .nav-drawer__title {
+    text-align: left;
+  }
 </style>
