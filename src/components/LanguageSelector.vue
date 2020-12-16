f94b8e1fe306076f38ec34f0ce2be11b0d5cb208
Alona <alona2156fr@gmail.com>
2020-12-16 15:37:47 +0200

    add: radio buttons with lang

---
+++ src/components/LanguageSelector.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container
+    class="px-0"
+    fluid
+  >
+    <v-radio-group v-model="selectedLang" mandatory>
+      <v-radio
+        v-for="(lang, index) in languages"
+        :key="index"
+        :label="lang.name"
+        :value="lang.locale"
+      ></v-radio>
+    </v-radio-group>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {
+  VContainer,
+  VRadioGroup,
+  VRadio,
+} from 'vuetify/lib';
+
+export default Vue.extend({
+  components: {
+    VContainer,
+    VRadioGroup,
+    VRadio,
+  },
+  data() {
+    return {
+      selectedLang: this.$i18n.locale,
+      languages: [{ name: `${this.$t('English')}`, locale: 'en' },
+        { name: `${this.$t('Russian')}`, locale: 'ru' },
+        { name: `${this.$t('Ukrainian')}`, locale: 'ua' }],
+    };
+  },
+});
+</script>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="settings">
     <h1 class="app__main__title">{{ $t("Settings") }}</h1>
+    <LanguageSelector />
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import LanguageSelector from '@/components/LanguageSelector.vue';
+
+export default Vue.extend({
+  components: {
+    LanguageSelector,
+  },
+});
+</script>
